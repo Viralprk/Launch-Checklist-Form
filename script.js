45f054738fb9462e3da5d18d9b9cc34e167c7416
Your Name <you@example.com>
2021-03-28 21:44:48 -0500

    assignment 5 updated code

--- script.js
+++ script.js
@@ -18,8 +18,31 @@ function isInvalidNumber(num) {
   return num != null && num.trim().length > 0 && isNaN(num);
 }
 
+function setMissionTarget(object) {
+  return `
+  <h2>Mission Destination</h2>
+<ol>
+   <li>Name: ${object.name}</li>
+   <li>Diameter: ${object.diameter}</li>
+   <li>Star: ${object.star}</li>
+   <li>Distance from Earth: ${object.distance}</li>
+   <li>Number of Moons: ${object.moons}</li>
+</ol>
+<img src="${object.image}"></img>`;
+}
+
 window.addEventListener("load", function () {
-  fetch("https://handlers.education.launchcode.org/static/planets.json");
+  fetch("https://handlers.education.launchcode.org/static/planets.json").then(
+    function (response) {
+      response.json().then(function (arr) {
+        const element = document.getElementById("missionTarget");
+        let index = Math.floor(Math.random() * Math.floor(arr.length));
+        let selectedMission = arr[index];
+        element.innerHTML = setMissionTarget(selectedMission);
+      });
+    }
+  );
+
   let form = document.querySelector("form");
   form.addEventListener("submit", function (event) {
     let validForm = true;
@@ -39,53 +62,58 @@ window.addEventListener("load", function () {
       validForm = false;
       alert("All fields are required !!");
     } else {
+      // let element = document.getElementById(id);
       document.getElementById("faultyItems").style.visibility = "visible";
-    }
-    if (isInvalidString(pilotNameInput)) {
-      validForm = false;
-    } else {
-      document.getElementById(
-        "pilotStatus"
-      ).innerHTML = `Pilot ${pilotNameInput} is ready for launch`;
-    }
-    if (isInvalidString(copilotNameInput)) {
-      validForm = false;
-    } else {
-      document.getElementById(
-        "copilotStatus"
-      ).innerHTML = `Co-pilot ${copilotNameInput} is ready for launch`;
-    }
-    if (isInvalidNumber(fuelLevelInput)) {
-      validForm = false;
-    } else {
-      document.getElementById("fuelStatus").innerHTML =
-        "Fuel level high enough for launch";
-      if (fuelLevelInput < 10000) {
-        validForm = false;
-        document.getElementById("launchStatus").innerHTML =
-          "Shuttle not ready for launch";
-        document.getElementById("launchStatus").style.color = "red";
-        document.getElementById("fuelStatus").innerHTML =
-          "There is not enough fuel for the journey";
-      }
-    }
-    if (isInvalidNumber(cargoMassInput)) {
-      validForm = false;
-    } else {
-      document.getElementById("cargoStatus").innerHTML =
-        "Cargo mass low enough for launch";
-      if (cargoMassInput > 10000) {
-        validForm = false;
-        document.getElementById("launchStatus").innerHTML =
-          "Shuttle not ready for launch";
-        document.getElementById("launchStatus").style.color = "red";
-        document.getElementById("cargoStatus").innerHTML =
-          "There is too much mass for the shuttle to take off";
-      }
-    }
 
-    if (!validForm) {
-      alert("Make sure to enter valid information for each field !!");
+      if (isInvalidString(pilotNameInput)) {
+        validForm = false;
+      } else {
+        document.getElementById(
+          "pilotStatus"
+        ).innerHTML = `Pilot ${pilotNameInput} is ready for launch`;
+      }
+      if (isInvalidString(copilotNameInput)) {
+        validForm = false;
+      } else {
+        document.getElementById(
+          "copilotStatus"
+        ).innerHTML = `Co-pilot ${copilotNameInput} is ready for launch`;
+      }
+      if (isInvalidNumber(fuelLevelInput)) {
+        validForm = false;
+      } else {
+        document.getElementById("fuelStatus").innerHTML =
+          "Fuel level high enough for launch";
+        if (fuelLevelInput < 10000) {
+          validForm = false;
+          document.getElementById("launchStatus").innerHTML =
+            "Shuttle not ready for launch";
+          document.getElementById("launchStatus").style.color = "red";
+          document.getElementById("fuelStatus").innerHTML =
+            "There is not enough fuel for the journey";
+        }
+      }
+      if (isInvalidNumber(cargoMassInput)) {
+        validForm = false;
+      } else {
+        document.getElementById("cargoStatus").innerHTML =
+          "Cargo mass low enough for launch";
+
+        if (cargoMassInput > 10000) {
+          validForm = false;
+          document.getElementById("launchStatus").innerHTML =
+            "Shuttle not ready for launch";
+          document.getElementById("launchStatus").style.color = "red";
+          document.getElementById("cargoStatus").innerHTML =
+            "There is too much mass for the shuttle to take off";
+        }
+      }
+
+      if (!validForm) {
+        alert("Make sure to enter valid information for each field !!");
+      }
+
+      //update fields
     }
 
     if (!validForm) {
